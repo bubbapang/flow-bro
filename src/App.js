bc215a40e889bbe42d0281117c5259e6575b04c3
bubbapang <adamtpangelinan@gmail.com>
2023-09-10 10:33:37 +1000

    First

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [workTime, setWorkTime] = useState(0);
+  const [playTime, setPlayTime] = useState(0);
+  const [workActive, setWorkActive] = useState(false);
+  const [playActive, setPlayActive] = useState(false);
+  const [ratio, setRatio] = useState(10); // For every 10 seconds of work, you get 1 second of play
+
+  useEffect(() => {
+    let workInterval, playInterval;
+
+    if (workActive) {
+      workInterval = setInterval(() => {
+        setWorkTime((prev) => prev + 1);
+        setPlayTime((prev) => prev + 1 / ratio);  // use the ratio here
+      }, 1000);
+    }
+
+    if (playActive) {
+      playInterval = setInterval(() => {
+        setPlayTime((prev) => prev - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(workInterval);
+      clearInterval(playInterval);
+    };
+  }, [workActive, playActive, ratio]); // Add ratio to dependency list
+
+  const toggleWork = () => {
+    setWorkActive(!workActive);
+    setPlayActive(false);
+  };
+
+  const togglePlay = () => {
+    if (playTime > 0) {
+      setPlayActive(!playActive);
+      setWorkActive(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Work Time: {workTime}s</h1>
+        <button onClick={toggleWork}>{workActive ? "Pause" : "Start"}</button>
+      </div>
+      <div>
+        <h1>Play Time: {Math.floor(playTime)}s</h1> {/* Math.floor to avoid decimal points */}
+        <button onClick={togglePlay}>{playActive ? "Pause" : "Start"}</button>
+      </div>
+      <div>
+        <h2>Work-to-Play Ratio</h2>
+        <input
+          type="number"
+          value={ratio}
+          onChange={(e) => setRatio(Number(e.target.value))}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
